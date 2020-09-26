be57d66f433f810ae8acec50e0b141c1e4207c1e
James McCann <jamesredux@gmail.com>
2020-09-26 16:29:59 +0100

    game engine done

--- src/Buttons.js
+++ src/Buttons.js
@@ -4,20 +4,24 @@ import { render } from '@testing-library/react';
 class Buttons extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      class: props.currentCard.class,
-    };
+
     console.log(props.currentCard);
     this.handleClick = this.handleClick.bind(this);
   }
 
+  // propbably don't need to create state above
+  // just use props.currendCard.class throughout
   handleClick(event) {
+    console.log(this.props.currentCard.class);
+    let result = false;
     const guess = event.target.dataset.class;
-    if (guess === this.state.class) {
+    if (guess === this.props.currentCard.class) {
       console.log('correct');
+      result = true;
     } else {
       console.log('wrong');
     }
+    this.props.handleGuess(result);
   }
 
   render() {
--- src/Game.js
+++ src/Game.js
@@ -6,22 +6,107 @@ class Game extends React.Component {
   constructor() {
     super();
     this.state = {
-      index: 14,
+      index: 0,
       score: 0,
+      total: Data.length - 1,
+      gameOver: false,
+      shuffleData: this.shuffleArray(Data),
     };
+    this.nextCard = this.nextCard.bind(this);
+    this.endGame = this.endGame.bind(this);
+    this.reStart = this.reStart.bind(this);
+  }
+
+  shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+
+  nextCard(result) {
+    console.log(this.state.index);
+    console.log('total');
+    console.log(this.state.total);
+    if (this.state.index >= this.state.total) {
+      this.endGame();
+    } else {
+      this.updateGame(result);
+    }
+  }
+
+  endGame() {
+    this.setState({ gameOver: true });
+    console.log('GAME OVER');
+  }
+
+  reStart() {
+    this.setState({
+      index: 0,
+      score: 0,
+      total: this.state.shuffleData.length - 1,
+      gameOver: false,
+    });
+  }
+
+  updateGame(result) {
+    if (result) {
+      this.setState((prevState) => {
+        return { index: prevState.index + 1, score: prevState.score + 1 };
+      });
+    } else {
+      this.setState((prevState) => {
+        return { index: prevState.index + 1 };
+      });
+    }
   }
 
   render() {
     const thisIndex = this.state.index;
-    return (
-      <div className='game-container'>
+    const cardList = this.state.shuffleData;
+    console.log(cardList);
+    let display;
+    if (this.state.gameOver !== true) {
+      display = (
         <div className='card-container'>
-          <div className='symbol-card'>{Data[thisIndex].symbol}</div>
+          <div className='symbol-card'>{cardList[thisIndex].symbol}</div>
           <div>
-            <Buttons currentCard={Data[thisIndex]} />
+            <Buttons
+              currentCard={cardList[thisIndex]}
+              handleGuess={this.nextCard}
+            />
           </div>
         </div>
-        <div className='game-data'>score etc</div>
+      );
+    } else {
+      display = (
+        <div className='card-container'>
+          <div className='results'>
+            <h1>Test Completed</h1>
+          </div>
+        </div>
+      );
+    }
+
+    return (
+      <div className='game-container'>
+        {display}
+        <div className='game-data'>
+          <div>
+            <p>
+              Score: {this.state.score} / {this.state.total + 1}
+            </p>
+          </div>
+          {this.state.gameOver ? (
+            <div className='restart' onClick={this.reStart}>
+              Restart?
+            </div>
+          ) : null}
+        </div>
       </div>
     );
   }
